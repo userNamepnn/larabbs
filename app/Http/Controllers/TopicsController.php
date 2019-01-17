5bd481a1062b93f299ff94ea9845995e0e4217e3
panninan <982397045@qq.com>
2019-01-17 10:54:36 +0800

    增加话题按规则排序

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,9 +14,9 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request)
 	{
-		$topics = Topic::with('user', 'category')->paginate(6);
+		$topics = Topic::withOrder($request->order)->paginate(6);
 		return view('topics.index', compact('topics'));
 	}
 
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,4 +15,44 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * 按需排序
+     * @param $query
+     * @param $order
+     * @return mixed
+     */
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order) {
+            case 'recent' :
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        return $query->with('user', 'category');
+    }
+
+    /**
+     * 按最近回复排序
+     * @param $query
+     * @return mixed
+     */
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    /**
+     * 按创建时间排序
+     * @param $query
+     * @return mixed
+     */
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
 }
