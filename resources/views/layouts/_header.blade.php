31f75c219c4bb5c9025dab60b2ad9785f2011f07
panninan <982397045@qq.com>
2019-01-20 03:02:40 +0000

    消息通知列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -34,13 +34,11 @@
             </a>
           </li>
           {{-- 消息通知标记 --}}
-                    <li>
-                        <a href="{{ route('notifications.index') }}" class="notifications-badge" style="margin-top: -2px;">
-                            <span class="badge badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'fade' }} " title="消息提醒">
-                                {{ Auth::user()->notification_count }}
-                            </span>
-                        </a>
-                    </li>
+          <li class="nav-item notification-badge">
+            <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white" href="{{ route('notifications.index') }}">
+              {{ Auth::user()->notification_count }}
+            </a>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <img src="{{ \Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
