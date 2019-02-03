45e2c2b0d9ce478771c99d1d0fcb63013abe27bb
潘倪楠 <982397045@qq.com>
2019-02-03 22:54:24 +0800

    首页&用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,12 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        //如果头像为空,给默认头像
+        if (empty($user->avatar)){
+            $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
 }
