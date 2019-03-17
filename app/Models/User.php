e3cfb2fd75b6c4ef27a966dd8362a54bc453ccbb
潘倪楠 <982397045@qq.com>
2019-03-17 17:19:16 +0800

    add weapp login

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,10 @@ class User extends Authenticatable implements MustVerifyEmailContract, JWTSubjec
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'avatar', 'introduction', 'phone', 'weixin_openid', 'weixin_unionid', 'registration_id'
+        'name', 'email', 'password', 'avatar', 'introduction', 'phone', 
+        'weixin_openid', 'weixin_unionid', 
+        'registration_id',
+        'weixin_session_key', 'weapp_openid',
     ];
 
     /**
